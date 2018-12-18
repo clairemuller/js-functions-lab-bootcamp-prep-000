451191dd5ec4f0b5a8a9601103b4ba49e159766b
Claire Muller <clairermuller@gmail.com>
2018-12-18 05:03:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,23 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 
+/*
 const happyHolidayTo = (holiday, name) => {
   return `Happy ${holiday}, ${name}!`;
 }
+*/
 
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+/*
 const holidayCountdown = (holiday, days) => {
   return `It's ${days} days until ${holiday}!`;
 }
+*/
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
+
