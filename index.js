99c846d25b88c9d8cb4cfece26b693666ed83c9e
Claire Muller <clairermuller@gmail.com>
2018-12-18 04:50:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // write your code below!
 
-const happyHolidays = 
+const happyHolidays = () => {
+  return "Happy holidays!"
+}
+
+const happyHolidaysTo = (name) => {
+  return `Happy holidays, ${name}!`
+}
+
+const happyHolidayTo = (holiday, name) => {
+  return `Happy ${holiday}, ${name}!`
 }
