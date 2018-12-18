4aab7095c2fbf8a156e98d7243ae56b6b2f7a51c
Claire Muller <clairermuller@gmail.com>
2018-12-18 04:50:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // write your code below!
 
 const happyHolidays = () => {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 
 const happyHolidaysTo = (name) => {
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
 
 const happyHolidayTo = (holiday, name) => {
-  return `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`;
 }
